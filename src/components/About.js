23532ba7481f907ee5211fa8087712f53f40c956
MonalKumar <monalshiv123@gmail.com>
2024-09-29 15:27:54 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,8 @@
+// const About=()=>{
+//     return(
+//         <div>
+//         <h3 >About Us page</h3>
+//         </div>
+//     )
+// }
+// export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+// const Home=()=>{
+//     return(
+//         <div>Homepage</div>
+//     )
+// }
+// <h1>hello world</h1>
+// export default Home;
