78d4dec72fe372741873f6e3bb438961fa3fc3f4
MonalKumar <monalshiv123@gmail.com>
2024-10-02 13:40:31 +0530

    destructuring the array of the components

--- src/App.js
+++ src/App.js
@@ -1586,15 +1586,15 @@ const resList= [
 
 
 
-const RestaurantCard = (props) => {
-  const {resData}= props;
+const RestaurantCard = ({resData}) => {
+  const {name,avgRating,cloudinaryImageId}= resData.info
   return (
     <div className="res-card">
-      <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+props.resData.info.cloudinaryImageId}
+      <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId}
         style={{ height: "195px" }}
       />
-      <h2>{props.resData.info.name}</h2>
-      <h2>{props.resData.info.avgRating}</h2>
+      <h2>{name}</h2>
+      <h2>{avgRating}</h2>
       <h2>cuisines</h2>
     </div>
   );
